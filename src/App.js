f334433315c233a0e7c23aaa6cc1e4ff19319f8c
sandip satyal <sandipsatyal25@gmail.com>
2020-09-20 23:43:16 +0545

    [style] navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 import SearchBox from "./components/searchbox";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <SearchBox />
     </div>
   );
