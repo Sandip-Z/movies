3d6a108d98b6c6951e4ac47b155916f8f1e2286f
sandip satyal <sandipsatyal25@gmail.com>
2020-09-22 23:31:46 +0545

    [add] onDragEnd method

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -3,10 +3,64 @@ import Watched from "./Watched";
 import ToWatch from "./ToWatch";
 import Watching from "./Watching";
 import { DragDropContext } from "react-beautiful-dnd";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  removeFromToWatch,
+  removeFromWatched,
+  removeFromWatching,
+} from "../../redux/Movies/action";
 
 const Body = () => {
-  const handleDragEnd = () => {
-    console.log("drag ended");
+  const dispatch = useDispatch();
+  const handleDragEnd = (dragged) => {
+    const draggedId = dragged.draggableId;
+    const sourceId = dragged.source.droppableId;
+    const destinationId = dragged.destination?.droppableId;
+    const destinationIndex = dragged.destination?.index;
+    const obj = {};
+
+    if (sourceId === destinationId) {
+      switch (sourceId) {
+        case "towatch":
+          // dispatch(rearrangeInToWatch(draggedId, dragged.destination.index))
+          break;
+        case "watching":
+          // dispatch(rearrangeInWatching(draggedId, dragged.destination.index))
+          break;
+        case "watched":
+          // dispatch(rearrangeInWatched(draggedId, dragged.destination.index))
+          break;
+      }
+    } else {
+      switch (destinationId) {
+        case "towatch":
+          // dispatch(migrateToToWatch(obj, destinationIndex))
+          break;
+        case "watching":
+          // dispatch(migrateToWatching(obj, destinationIndex))
+          break;
+        case "watched":
+          // dispatch(migrateToWatched(obj, destinationIndex))
+          break;
+        default:
+          break;
+      }
+    }
+
+    if (dragged.destination) {
+      switch (sourceId) {
+        case "towatch":
+          dispatch(removeFromToWatch(draggedId));
+          break;
+        case "watching":
+          dispatch(removeFromWatching(draggedId));
+          break;
+        case "watched":
+          dispatch(removeFromWatched(draggedId));
+          break;
+      }
+    }
+    console.log(dragged);
   };
   return (
     <DragDropContext onDragEnd={handleDragEnd}>
