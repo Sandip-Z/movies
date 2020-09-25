fa2ada188381889035569fbcad9c3b1a17980046
sandip satyal <sandipsatyal25@gmail.com>
2020-09-25 23:01:31 +0545

    [add] function to rearrange array index

---
+++ src/utils/arrays.js
@@ -0,0 +1,20 @@
+export function rearrange(arr, from, to) {
+  return arr.reduce((prev, current, idx, self) => {
+    if (from === to) {
+      prev.push(current);
+    }
+    if (idx === from) {
+      return prev;
+    }
+    if (from < to) {
+      prev.push(current);
+    }
+    if (idx === to) {
+      prev.push(self[from]);
+    }
+    if (from > to) {
+      prev.push(current);
+    }
+    return prev;
+  }, []);
+}
