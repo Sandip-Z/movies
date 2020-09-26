7e632dc2804658307a65c4223d04bc2087d97ce0
sandip satyal <sandipsatyal25@gmail.com>
2020-09-26 22:04:26 +0545

    [change] droppable zone bg-color

--- src/hooks/useClickListener.js
+++ src/hooks/useClickListener.js
@@ -4,18 +4,22 @@ const useClickListener = (id, toggle) => {
   const [focused, setFocused] = useState(false);
   useEffect(() => {
     const targetedElement = document.querySelector(`#${id}`);
+    console.log("at use effect");
     document.body.addEventListener("click", (e) => {
+      console.log("at click body");
       if (targetedElement.contains(e.target)) {
         setFocused(true);
+        console.log("at set true");
       } else {
         setFocused(false);
+        console.log("at set false");
       }
-      // console.log(focused, targetedElement.contains(e.target));
       if (toggle && focused && targetedElement.contains(e.target)) {
         setFocused(false);
+        console.log("at toggle");
       }
     });
-  });
+  }, []);
   return [
     focused,
     (value) => {
