5e8e3bcd5ce623b39b59f477e312537ef60e5591
sandip satyal <sandipsatyal25@gmail.com>
2020-09-27 01:17:05 +0545

    [add] markup for login page

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,39 @@
 import React from "react";
 
 const LoginPage = () => {
-  return <p>This is login page</p>;
+  return (
+    <div className="login d-flex flex-column justify-content-center shadow-lg">
+      <div className="row">
+        <div className="col-lg-6 border">image here</div>
+        <div className="col-lg-6">
+          <h3 className="text-center">Member Login</h3>
+          <form className="login-form">
+            <section>
+              <label htmlFor="email">Email</label>
+              <input name="email" type="email" />
+            </section>
+            <section>
+              <label htmlFor="password">Password</label>
+              <input name="password" type="password" />
+            </section>
+            <input
+              type="submit"
+              value="Login"
+              className="d-flex m-auto btn btn-primary"
+            />
+          </form>
+          <div className="d-flex flex-column align-items-center my-3">
+            <a href="#" className="mb-3">
+              Forgot Password
+            </a>
+            <span>
+              Dont have an account? <a href="#">Create your account</a>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
