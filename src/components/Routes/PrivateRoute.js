a6b7495e3a45f03a9037ad69576a6beb5d9d5c1b
sandip satyal <sandipsatyal25@gmail.com>
2020-10-01 22:22:20 +0545

    [add] private and public route component

---
+++ src/components/Routes/PrivateRoute.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
+import PrivateLayout from "../Layout/PrivateLayout";
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const isLoggedin = () => {
+    return true;
+  };
+
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        isLoggedin() ? (
+          <PrivateLayout>
+            <Component {...props} />
+          </PrivateLayout>
+        ) : (
+          <Redirect to="/login" />
+        )
+      }
+    />
+  );
+};
+
+export default PrivateRoute;
