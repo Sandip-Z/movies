6dc1af0fcf64165cf5dba628e64cf2a9d0a3f4e0
sandip satyal <sandipsatyal25@gmail.com>
2020-10-01 23:04:56 +0545

    [add] memoized sidebar menu arrays

--- src/components/SideNavbar/SidebarMenu.js
+++ src/components/SideNavbar/SidebarMenu.js
@@ -1,26 +1,59 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useMemo } from "react";
+import { Link, useLocation } from "react-router-dom";
 import { BsSearch } from "react-icons/bs";
 import { AiFillDashboard, AiOutlineLogout } from "react-icons/ai";
 import { RiRunFill, RiProfileFill } from "react-icons/ri";
 
+const userInformation = [
+  { name: "Profile", icon: <RiProfileFill />, href: "/profile" },
+  { name: "Logout", icon: <AiOutlineLogout />, href: "/logout" },
+];
+
+const menus = [
+  { name: "Dashboard", icon: <AiFillDashboard />, href: "/dashboard" },
+  { name: "Current Marathon", icon: <RiRunFill />, href: "/current-marathon" },
+];
+
 const SidebarMenu = () => {
+  const location = useLocation();
+
+  const memoizedMenu = useMemo(() => {
+    const renderMenus = menus.map((menu) => {
+      return (
+        <li
+          key={menu.href}
+          className={menu.href === location.pathname ? "active" : ""}
+        >
+          {menu.icon}{" "}
+          <span className="mx-2">
+            <Link to={menu.href}>{menu.name}</Link>
+          </span>
+        </li>
+      );
+    });
+    return renderMenus;
+  }, [menus, location]);
+
+  const memoizedUserInformation = useMemo(() => {
+    const renderUserInformation = userInformation.map((userinfo) => {
+      return (
+        <li
+          key={userinfo.href}
+          className={userinfo.href === location.pathname ? "active" : ""}
+        >
+          {userinfo.icon}
+          <span className="mx-2">
+            <Link to={userinfo.href}>{userinfo.name}</Link>
+          </span>
+        </li>
+      );
+    });
+    return renderUserInformation;
+  }, [userInformation, location]);
+
   return (
     <div className="side-navbar__menu">
-      <ul className="sidebar-navbar__menu--list">
-        <li>
-          <AiFillDashboard />
-          <span className="mx-2">
-            <Link to="/dashboard">Dashboard</Link>
-          </span>
-        </li>
-        <li className="active">
-          <RiRunFill />
-          <span className="mx-2">
-            <Link to="current-marathon">Current Marathon</Link>
-          </span>
-        </li>
-      </ul>
+      <ul className="sidebar-navbar__menu--list">{memoizedMenu}</ul>
       <div className="d-flex justify-content-between px-3">
         <p className="sidebar-title">Archieves</p>
         <div>
@@ -34,17 +67,7 @@ const SidebarMenu = () => {
         <p className="sidebar-title">User Information</p>
       </div>
       <ul className="sidebar-navbar__menu--list pt-2">
-        <li>
-          {" "}
-          <RiProfileFill />{" "}
-          <span className="mx-2">
-            <Link to="/profile">My Profile</Link>
-          </span>
-        </li>
-        <li>
-          <AiOutlineLogout />
-          <span className="mx-2">Log out</span>
-        </li>
+        {memoizedUserInformation}
       </ul>
     </div>
   );
